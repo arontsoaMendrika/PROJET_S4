d2fe298a6f6f9eb5fa1b41aa18482b49b278df41
arontsoaMendrika <mendrikaarontsoa@gmail.com>
2026-05-09 14:23:53 +0300

    Insertion 15 codes

--- schema_regimes_wallet.sql
+++ schema_regimes_wallet.sql
@@ -32,3 +32,20 @@ CREATE TABLE transactions (
     PRIMARY KEY (id),
     CONSTRAINT fk_transaction_wallet FOREIGN KEY (wallet_id) REFERENCES user_wallet(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
+
+INSERT INTO recharge_codes (code_value, amount) VALUES
+('REG001', 10.00),
+('REG002', 20.00),
+('REG003', 50.00),
+('REG004', 100.00),
+('REG005', 200.00),
+('REG006', 500.00),
+('REG007', 1000.00),
+('REG008', 2000.00),
+('REG009', 5000.00),
+('REG010', 10000.00),
+('REG011', 20000.00),
+('REG012', 50000.00),
+('REG013', 100000.00),
+('REGG014', 200000.00),
+('REGG015', 500000.00);
